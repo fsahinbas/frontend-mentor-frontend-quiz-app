d5ee293e763e39e732b5ab5479f6c0bbe1cc0529
Fatih ŞAHİNBAŞ <fsahinbas@gmail.com>
2024-08-23 23:57:01 +0300

    suspense...

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -23,46 +23,48 @@ const ResultPage = () => {
           bgColor={`${bg}`}
         />
       </Suspense>
-      <main className="main">
-        <div className="left-side">
-          <h1 className="headingTwoLines">
-            <span className="heading-lg">Quiz completed</span>
-            <span className="heading-lg-bold">You scored...</span>
-          </h1>
-          <p className="body-s"></p>
-        </div>
-        <div className={styles.rightSide}>
-          <div className="result">
-            <div className={styles.header}>
-              <div
-                style={{ backgroundColor: `#${bg}` }}
-                className={styles.imageWrapper}
-              >
-                <Image
-                  src={`/assets/images/icon-${quiz?.toLowerCase()}.svg`}
-                  height={40}
-                  width={40}
-                  alt=""
-                />
-              </div>
-              <p className="heading-sm">{quiz}</p>
-            </div>
-
-            <p
-              className="heading-lg-bold"
-              style={{ color: "var(--dark-navy)" }}
-            >
-              {score}
-            </p>
-            <p className="body-m" style={{ color: "var(--grey-navy)" }}>
-              out of 10
-            </p>
+      <Suspense>
+        <main className="main">
+          <div className="left-side">
+            <h1 className="headingTwoLines">
+              <span className="heading-lg">Quiz completed</span>
+              <span className="heading-lg-bold">You scored...</span>
+            </h1>
+            <p className="body-s"></p>
           </div>
-          <button className={`${styles.btnSubmit} heading-sm`}>
-            <Link href={"/"}>Play Again</Link>
-          </button>
-        </div>
-      </main>
+          <div className={styles.rightSide}>
+            <div className="result">
+              <div className={styles.header}>
+                <div
+                  style={{ backgroundColor: `#${bg}` }}
+                  className={styles.imageWrapper}
+                >
+                  <Image
+                    src={`/assets/images/icon-${quiz?.toLowerCase()}.svg`}
+                    height={40}
+                    width={40}
+                    alt=""
+                  />
+                </div>
+                <p className="heading-sm">{quiz}</p>
+              </div>
+
+              <p
+                className="heading-lg-bold"
+                style={{ color: "var(--dark-navy)" }}
+              >
+                {score}
+              </p>
+              <p className="body-m" style={{ color: "var(--grey-navy)" }}>
+                out of 10
+              </p>
+            </div>
+            <button className={`${styles.btnSubmit} heading-sm`}>
+              <Link href={"/"}>Play Again</Link>
+            </button>
+          </div>
+        </main>
+      </Suspense>
     </>
   );
 };
